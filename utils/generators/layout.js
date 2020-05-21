d8740e34639da650c9479c7cd07c4015ec4df565
Killin <subikrus@gmail.com>
2020-05-21 19:29:57 +0700

    добавил генерацию шаблона и страниц

--- utils/generators/layout.js
+++ utils/generators/layout.js
@@ -13,14 +13,36 @@ module.exports = function(plop) {
 		    },
 			{
 				type: 'confirm',
+				name: 'isDefaultStyles',
+				message: 'Использовать дефолтные стили для этого шаблона?',
+				default: true
+			},
+			{
+				type: 'confirm',
 				name: 'isNewHeader',
-				message: 'Создать для этого шаблона новый header?'
+				message: 'Создать для этого шаблона новый header?',
+				default: false
 		    },
 			{
 				type: 'confirm',
 				name: 'isNewFooter',
-				message: 'Создать для этого шаблона новый footer?'
-		    }
+				message: 'Создать для этого шаблона новый footer?',
+				default: false
+		    },
+			{
+				type: 'confirm',
+				name: 'isNewPage',
+				message: 'Создать новую страницу для этого шаблона?',
+				default: false
+			},
+			{
+				type: 'input',
+				name: 'page_name',
+				message: 'Введите название страницы',
+				when(answers) {
+					return answers.isNewPage
+				}
+			}
 		],
 		actions: [
 			{
@@ -43,6 +65,15 @@ module.exports = function(plop) {
 				templateFile: '../templates/layout/footer.hbs',
 				skip(data) {
 					if (data.isNewFooter) return
+					else return 'skip'
+				}
+			},
+			{
+				type: 'add',
+				path: '../../src/app/pages/{{page_name}}.pug',
+				templateFile: '../templates/page.hbs',
+				skip(data) {
+					if (data.isNewPage) return
 					else return 'skip'
 				}
 			}
